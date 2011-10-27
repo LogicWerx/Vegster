c910a002b5e945caf1cf029e2f0c239c7f2f1b87
Matt Holloway <matthew.holloway@logicwerx.com>
2011-10-26 20:45:12 -0400

    Initialization of Vegster - initial setup of the framwork

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'gravatar_image_tag'
+gem 'will_paginate', '~> 3.0.2'
+
+group :development do
+	gem 'rspec-rails', '2.0.1'
+	gem 'annotate-models', '1.0.4'
+	gem 'faker', '0.3.1'
+end
+
+group :test do
+	gem 'rspec', '2.0.1'
+	gem 'webrat', '0.7.1'
+	gem 'spork', '0.9.0.rc5'
+	gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vegster::Application.config.secret_token = 'd24376efc7514893a436907ba6a8c0efe8e66f2c3a7db4ca1a9bba58c9269d7cbcf2a38edaae30a8f2a1567654db9852ed1b0018702c79c1c1d2c13e7e923786'
